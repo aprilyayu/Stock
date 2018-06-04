b2f25c5cbcd0dbba134e9f8d4d74382ec20d43a6
WIN.10 <aprilyayupooh@gmail.com>
2018-06-04 22:38:13 +0700

    Initial commit

---
+++ app/src/main/java/com/ace/san/db/ItemList.java
@@ -0,0 +1,50 @@
+package com.ace.san.db;
+
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import com.ace.san.R;
+import com.ace.san.db.Item;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+
+public class ItemList extends ArrayAdapter<Item> {
+    private Activity context;
+    List<Item> items;
+
+
+    public ItemList(Activity context, List<Item> items) {
+        super(context, R.layout.list, items);
+        this.context = context;
+        this.items = items;
+
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.activity_itemlist, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewName);
+        TextView textViewCat = (TextView) listViewItem.findViewById(R.id.textViewCat);
+
+        Item item = items.get(position);
+        textViewName.setText(item.getName());
+        textViewCat.setText(item.getCategory());
+
+
+        return listViewItem;
+    }
+
+}
